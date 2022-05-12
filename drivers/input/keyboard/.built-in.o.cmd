e03cfe75d308461690425fbcbfc92c12e29d3d47
Dave Milici <dave@joinloop.com>
2022-05-12 14:42:02 -0700

    Remove intermediates supposed to be ignored by git.

--- drivers/input/keyboard/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/input/keyboard/built-in.o :=  ./../prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-ld -EL    -r -o drivers/input/keyboard/built-in.o drivers/input/keyboard/adc-keys.o drivers/input/keyboard/adc-keys-chan0.o drivers/input/keyboard/rk_keys.o 
